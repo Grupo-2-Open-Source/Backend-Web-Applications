96e612e1f51d6fdcfffb6edc598e551179372569
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-10-20 21:36:23 -0500

    shore:primer commit

---
+++ src/main/java/com/example/backendwebapplications2/BackendWebApplications2Application.java
@@ -0,0 +1,13 @@
+package com.example.backendwebapplications2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebApplications2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWebApplications2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendwebapplications2/BackendWebApplications2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendwebapplications2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebApplications2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
